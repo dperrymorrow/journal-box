510e6a3188b3004572cb9a1c86431348e696011e
dperrymorrow <dperrymorrow@gmail.com>
2018-06-20 16:01:17 -0700

    redirect to google if inactive for 2 minutes

--- source/app/components/action-bar.vue
+++ source/app/components/action-bar.vue
@@ -1,26 +1,31 @@
 <template lang="pug">
   .action-bar
-    button(@click.prevent="back")
-      i.fas.fa-angle-left
-    button.icon(@click.prevent="today", v-if="!$store.getters['dates/isToday']")
-      i.fas.fa-dot-circle
-      | Today
+    .btn-group
+      button(@click.prevent="back")
+        i.fas.fa-angle-left
+      button(@click.prevent="showCalendar")
+        i.fas.fa-calendar-alt
+
 
     .btn-group
       button.icon(@click.prevent="logout")
         i.fas.fa-user-alt-slash
-        | Logout
+
+      button(@click.prevent="$store.commit('ui/toggleShy')")
+        i.fas.fa-eye(v-if="$store.state.ui.isShy")
+        i.fas.fa-eye-slash(v-else)
 
       button(@click.prevent="save")
         i.fas.fa-save
 
 
-    button(@click.prevent="$store.commit('ui/toggleShy')")
-      i.fas.fa-eye(v-if="$store.state.ui.isShy")
-      i.fas.fa-eye-slash(v-else)
+    .btn-group
+      button.icon(@click.prevent="today", v-if="!$store.getters['dates/isToday']")
+        i.fas.fa-dot-circle
+        | Today
+      button(@click.prevent="next", :disabled="$store.getters['dates/isToday']")
+        i.fas.fa-angle-right
 
-    button(@click.prevent="next", v-if="!$store.getters['dates/isToday']")
-      i.fas.fa-angle-right
 </template>
 
 <script>
@@ -35,6 +40,8 @@ export default {
       this.$store.dispatch("files/loadCurrent");
     },
 
+    showCalendar() {},
+
     next() {
       this.$store.commit("dates/setCurrent", this.$store.getters["dates/nextSlug"]);
       this.$store.dispatch("files/loadCurrent");
@@ -46,7 +53,8 @@ export default {
     },
 
     logout() {
-      session.destroy();
+      if (window.confirm("sure you wanna logout? You will loose unsaved work on your entry."))
+        session.destroy();
     },
 
     save() {
